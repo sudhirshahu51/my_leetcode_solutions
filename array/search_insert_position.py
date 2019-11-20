e7d1050ce581360b03a0e0181419afa45eb3ee1f
sudhir <sudhirkumarshahu80@gmail.com>
2019-11-20 16:09:51 +0530

    search_insert_position

---
+++ array/search_insert_position.py
@@ -0,0 +1,14 @@
+#problem link https://leetcode.com/problems/search-insert-position/
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if nums:
+            if target in nums:
+                return int(nums.index(target))
+            elif nums[(len(nums) -1)] < target:
+                return len(nums)
+            else:
+                for i in range(len(nums)):
+                    if target < nums[i]:
+                        return i
+        else: return 0
